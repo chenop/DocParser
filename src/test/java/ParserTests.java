86ee945a596ba0cf9bcf76814d7df29d491c0c38
Chen.Oppenhaim <chenop@gmail.com>
2015-12-20 00:04:17 +0200

    Adding Test infrastructure

---
+++ src/test/java/ParserTests.java
@@ -0,0 +1,40 @@
+import com.chenop.ParserManager;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by Chen.Oppenhaim on 12/19/2015.
+ */
+public class ParserTests {
+
+    public static final String DOCX_FILE_PATH = "sample.docx";
+    public static final String DOC_FILE_PATH = "word97.doc";
+
+    @Test
+    public void parsingDocxShouldGetTextTest() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(DOCX_FILE_PATH);
+
+        String text = ParserManager.parse(inputStream);
+
+        System.out.println(text);
+        Assert.assertThat(text.isEmpty(), is(false));
+    }
+
+    @Test
+    public void parsingDocShouldGetTextTest() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(DOC_FILE_PATH);
+
+        String text = ParserManager.parse(inputStream);
+
+        System.out.println(text);
+        Assert.assertThat(text.isEmpty(), is(false));
+    }
+
+
+
+}
