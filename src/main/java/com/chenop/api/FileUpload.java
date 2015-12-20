1a0a38de7cc7e85d87f361c4aa3d20a3e057604f
Chen.Oppenhaim <chenop@gmail.com>
2015-12-20 02:05:12 +0200

    Analyze keywords

--- src/main/java/com/chenop/api/FileUpload.java
+++ src/main/java/com/chenop/api/FileUpload.java
@@ -1,7 +1,7 @@
 package com.chenop.api;
 
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import com.chenop.KeywordsAnalyzer;
+import com.chenop.ParserManager;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
@@ -10,7 +10,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.*;
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * Created by Chen.Oppenhaim on 11/18/2015.
@@ -28,11 +29,9 @@ public class FileUpload {
     public Response uploadPdfFile(  @FormDataParam("file") InputStream fileInputStream,
                                     @FormDataParam("file") FormDataContentDisposition fileMetaData) throws Exception
     {
-        XWPFDocument document = new XWPFDocument(fileInputStream);
-        XWPFWordExtractor wordExtractor = new XWPFWordExtractor(document);
-        String text = wordExtractor.getText();
-        System.out.println("result: " + text);
+        String text = ParserManager.parse(fileInputStream);
+        List<String> keywords = KeywordsAnalyzer.extractKeywords(text);
 
-        return Response.ok("result: " + text).build();
+        return Response.ok(keywords).build();
     }
 }
