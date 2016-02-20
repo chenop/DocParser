bb73880df05c25e600b4e3008be32f49cf403382
Chen <chenop@gmail.com>
2016-02-20 11:23:37 +0200

    - Adding Base64 conversion (First Heroku deploy)

--- src/main/java/com/chenop/api/FileUpload.java
+++ src/main/java/com/chenop/api/FileUpload.java
@@ -3,6 +3,7 @@ package com.chenop.api;
 import com.chenop.DocAnalyzer;
 import com.chenop.ParserManager;
 import com.chenop.models.CVData;
+import org.apache.commons.codec.binary.Base64;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
@@ -12,6 +13,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -32,8 +35,21 @@ public class FileUpload {
                                @FormDataParam("file") FormDataContentDisposition fileMetaData) throws Exception
     {
         String text = ParserManager.parse(fileInputStream);
+        if (isBase64(text))
+            text = decodeBase64ToText(text);
         CVData cvData = DocAnalyzer.extractCVData(text);
 
         return Response.ok(cvData).build();
+    }
+
+    private boolean isBase64(String text) {
+        String base64String = text.split(",")[1]; // remove the first part of the string
+        return Base64.isBase64(base64String);
+    }
+
+    private String decodeBase64ToText(String text) throws IOException {
+        byte[] bytes = Base64.decodeBase64(text.split(",")[1]);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+        return ParserManager.parse(inputStream);
     }
 }
